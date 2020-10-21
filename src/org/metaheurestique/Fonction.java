da0af1549cd32441fee371218c8587d93ce03b9c
Tenalic <43466853+Tenalic@users.noreply.github.com>
2020-10-21 10:24:20 +0200

    Init
    Init

---
+++ src/org/metaheurestique/Fonction.java
@@ -0,0 +1,12 @@
+package org.metaheurestique;
+
+public class Fonction {
+
+	void sauvegarde_donnees(String name) {
+
+	}
+
+	Item charge_donnees(String name) {
+		return null;
+	}
+}
---
+++ src/org/metaheurestique/Item.java
@@ -0,0 +1,13 @@
+package org.metaheurestique;
+
+public class Item {
+
+	private int[] p;
+	private int[] w;
+
+	public Item() {
+		this.p = new int[3];
+		this.w = new int[3];
+	}
+
+}
